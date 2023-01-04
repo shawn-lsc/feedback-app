8149208b52b5bdff503c140543c07e15371d7110
shawn-lsc <shawn_lsc@outlook.com>
2023-01-04 16:36:47 +0800

    ch3 init

--- src/App.js
+++ src/App.js
@@ -1,35 +1,7 @@
 function App() {
-  const title = "blog post";
-  const body = "this is my blog post";
-  const comments = [
-    { id: 1, text: "comment 1" },
-    { id: 2, text: "comment 2" },
-    { id: 3, text: "comment 3" },
-  ];
-  const loading = false;
-  const showComments = true;
-
-  if (loading) {
-    return <h1>loading...</h1>;
-  }
-
-  const commentBlcok = (
-    <div className='comments'>
-      <h3>Comments ({comments.length})</h3>
-      <ul>
-        {comments.map((comment, index) => (
-          <li key={index}>{comment.text}</li>
-        ))}
-      </ul>
-    </div>
-  );
-
   return (
     <div className='container'>
-      <h1>{title.toUpperCase()}</h1>
-      <p>{body}</p>
-
-      {showComments && commentBlcok}
+      <h1>My App</h1>
     </div>
   );
 }
