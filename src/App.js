61f00c6439655279e01abaf3d6633ee52abfbca8
shawn-lsc <shawn_lsc@outlook.com>
2023-01-04 16:34:36 +0800

    ch1 & ch2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const title = "blog post";
+  const body = "this is my blog post";
+  const comments = [
+    { id: 1, text: "comment 1" },
+    { id: 2, text: "comment 2" },
+    { id: 3, text: "comment 3" },
+  ];
+  const loading = false;
+  const showComments = true;
+
+  if (loading) {
+    return <h1>loading...</h1>;
+  }
+
+  const commentBlcok = (
+    <div className='comments'>
+      <h3>Comments ({comments.length})</h3>
+      <ul>
+        {comments.map((comment, index) => (
+          <li key={index}>{comment.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>{title.toUpperCase()}</h1>
+      <p>{body}</p>
+
+      {showComments && commentBlcok}
     </div>
   );
 }
